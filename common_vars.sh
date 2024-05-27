a5aa70cee7845b8433da840228f8c57e0b5d033b
Francis Pouatcha <francis.pouatcha@adorsys.com>
2024-05-27 08:31:22 +0100

    Improved dev path
    Signed-off-by: Francis Pouatcha <francis.pouatcha@adorsys.com>

--- common_vars.sh
+++ common_vars.sh
@@ -8,8 +8,9 @@ TARGET_DIR=$WORK_DIR/target
 TOOLS_DIR=$TARGET_DIR/tools
 
 # Dev dir where to clone keycloak
+# KC_TARGET_BRANCH=main
 KC_TARGET_BRANCH=target-20240525-0
-KC_OID4VCI=$KC_TARGET_BRANCH
+KC_OID4VCI="keycloak_"$KC_TARGET_BRANCH
 
 # Ensure all sensitive data like passwords and keys are passed through environment variables or secure stores.
 KEYCLOAK_ADMIN=admin
