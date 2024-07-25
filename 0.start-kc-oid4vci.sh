d001dd577ed196d298ea067ec6dbd5f5868946de
Dylane Bengono <chaneldylanebengono@gmail.com>
2024-07-25 10:33:03 +0100

    chore: Improve Keycloak shutdown logic and check keycloak running with oid4vc-vci
    Refactor the code to determine the operating system and shutdown Keycloak accordingly. This ensures that Keycloak is properly stopped before starting a new instance.

--- 0.start-kc-oid4vci.sh
+++ 0.start-kc-oid4vci.sh
@@ -45,11 +45,20 @@ else
 fi
 
 # Shutdown keycloak if any
-keycloak_pid=$(ps aux | grep -i '[k]eycloak' | awk '{print $2}')
-if [ -n "$keycloak_pid" ]; then
-    echo "A Keycloak instance is already running (PID: $keycloak_pid). Shutting it down..."
-    kill $keycloak_pid 
-fi
+# Determine OS platform and shutdown Keycloak if running
+OS=$(uname -s)
+case "$OS" in
+    Linux*|Darwin*)
+        keycloak_pid=$(pgrep -f keycloak)
+        if [ -n "$keycloak_pid" ]; then
+            echo "Keycloak instance found (PID: $keycloak_pid) on $OS. Shutting it down..."
+            kill $keycloak_pid
+        fi
+        ;;
+    *)
+        echo "This script supports only Linux or macOS."
+        ;;
+esac
 
 # Change to the tools directory and unpack keycloak
 if [ -d "$KC_INSTALL_DIR" ]; then
