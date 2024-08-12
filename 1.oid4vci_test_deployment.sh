bbf67dc34daaa87dd5cda624e7e704a97f7c0538
Ingrid Kamga <Ingrid.Kamga@adorsys.com>
2024-08-12 15:32:15 +0100

    Reuse issuer keystore

--- 1.oid4vci_test_deployment.sh
+++ 1.oid4vci_test_deployment.sh
@@ -49,66 +49,66 @@ echo "Generated RS256 key will be disbled... KID=$RS256_KID PROV_ID=$RS256_PROV_
 # AES_PROV_ID=$($KC_INSTALL_DIR/bin/kcadm.sh get keys | jq --arg kid "$AES_KID" '.keys[] | select(.kid == $kid)' | jq -r '.providerId')
 # echo "Generated AES key will be disbled... KID=$AES_KID PROV_ID=$AES_PROV_ID"
 
-# Delete keystore if one exists
-# change into keycloak directory & build keycloak
+# Reuse keystore if one exists
 if [ -f "$KEYCLOAK_KEYSTORE_FILE" ]; then
-    echo "File $KEYCLOAK_KEYSTORE_FILE exists, will be deleted..."
-    rm "$KEYCLOAK_KEYSTORE_FILE"
+    echo "File $KEYCLOAK_KEYSTORE_FILE exists, will be reused..."
+else
+    # Generate a keypairs into a PKCS12 keystore using java. We prefer an external file, as content will be shared among servers.
+    echo "Generating $KEYCLOAK_KEYSTORE_FILE..."
+    
+    keytool \
+      -genkeypair \
+      -keyalg EC \
+      -keysize 256 \
+      -keystore $KEYCLOAK_KEYSTORE_FILE \
+      -storepass $KEYCLOAK_KEYSTORE_PASSWORD \
+      -alias $KEYCLOAK_KEYSTORE_ECDSA_KEY_ALIAS \
+      -keypass $KEYCLOAK_KEYSTORE_PASSWORD \
+      -storetype $KEYCLOAK_KEYSTORE_TYPE \
+      -dname "CN=ECDSA Signing Key, OU=Keycloak Competence Center, O=Adorsys Lab, L=Bangante, ST=West, C=Cameroon"
+
+    keytool \
+      -genkeypair \
+      -keyalg RSA \
+      -keysize 3072 \
+      -keystore $KEYCLOAK_KEYSTORE_FILE \
+      -storepass $KEYCLOAK_KEYSTORE_PASSWORD \
+      -alias $KEYCLOAK_KEYSTORE_RSA_SIG_KEY_ALIAS \
+      -keypass $KEYCLOAK_KEYSTORE_PASSWORD \
+      -storetype $KEYCLOAK_KEYSTORE_TYPE \
+      -dname "CN=RSA Signing Key, OU=Keycloak Competence Center, O=Adorsys Lab, L=Bangante, ST=West, C=Cameroon" 
+
+    keytool \
+      -genkeypair \
+      -keyalg RSA \
+      -keysize 3072 \
+      -keystore $KEYCLOAK_KEYSTORE_FILE \
+      -storepass $KEYCLOAK_KEYSTORE_PASSWORD \
+      -alias $KEYCLOAK_KEYSTORE_RSA_ENC_KEY_ALIAS \
+      -keypass $KEYCLOAK_KEYSTORE_PASSWORD \
+      -storetype $KEYCLOAK_KEYSTORE_TYPE \
+      -dname "CN=RSA Encryption Key, OU=Keycloak Competence Center, O=Adorsys Lab, L=Bangante, ST=West, C=Cameroon" 
+
+    # keytool \
+    #   -genseckey \
+    #   -keyalg HmacSHA512 \
+    #   -keysize 512 \
+    #   -keystore $KEYCLOAK_KEYSTORE_FILE \
+    #   -storepass $KEYCLOAK_KEYSTORE_PASSWORD \
+    #   -alias $KEYCLOAK_KEYSTORE_HMAC_SIG_KEY_ALIAS \
+    #   -keypass $KEYCLOAK_KEYSTORE_PASSWORD \
+    #   -storetype $KEYCLOAK_KEYSTORE_TYPE
+
+    # keytool \
+    #   -genseckey \
+    #   -keyalg AES \
+    #   -keysize 256 \
+    #   -keystore $KEYCLOAK_KEYSTORE_FILE \
+    #   -storepass $KEYCLOAK_KEYSTORE_PASSWORD \
+    #   -alias $KEYCLOAK_KEYSTORE_AES_ENC_KEY_ALIAS \
+    #   -keypass $KEYCLOAK_KEYSTORE_PASSWORD \
+    #   -storetype $KEYCLOAK_KEYSTORE_TYPE
 fi
-
-# Generate a keypairs into a PKCS12 keystore using java. We prefer an external file, as content will be shared among servers.
-keytool \
-  -genkeypair \
-  -keyalg EC \
-  -keysize 256 \
-  -keystore $KEYCLOAK_KEYSTORE_FILE \
-  -storepass $KEYCLOAK_KEYSTORE_PASSWORD \
-  -alias $KEYCLOAK_KEYSTORE_ECDSA_KEY_ALIAS \
-  -keypass $KEYCLOAK_KEYSTORE_PASSWORD \
-  -storetype $KEYCLOAK_KEYSTORE_TYPE \
-  -dname "CN=ECDSA Signing Key, OU=Keycloak Competence Center, O=Adorsys Lab, L=Bangante, ST=West, C=Cameroon"
-
-keytool \
-  -genkeypair \
-  -keyalg RSA \
-  -keysize 3072 \
-  -keystore $KEYCLOAK_KEYSTORE_FILE \
-  -storepass $KEYCLOAK_KEYSTORE_PASSWORD \
-  -alias $KEYCLOAK_KEYSTORE_RSA_SIG_KEY_ALIAS \
-  -keypass $KEYCLOAK_KEYSTORE_PASSWORD \
-  -storetype $KEYCLOAK_KEYSTORE_TYPE \
-  -dname "CN=RSA Signing Key, OU=Keycloak Competence Center, O=Adorsys Lab, L=Bangante, ST=West, C=Cameroon" 
-
-keytool \
-  -genkeypair \
-  -keyalg RSA \
-  -keysize 3072 \
-  -keystore $KEYCLOAK_KEYSTORE_FILE \
-  -storepass $KEYCLOAK_KEYSTORE_PASSWORD \
-  -alias $KEYCLOAK_KEYSTORE_RSA_ENC_KEY_ALIAS \
-  -keypass $KEYCLOAK_KEYSTORE_PASSWORD \
-  -storetype $KEYCLOAK_KEYSTORE_TYPE \
-  -dname "CN=RSA Encryption Key, OU=Keycloak Competence Center, O=Adorsys Lab, L=Bangante, ST=West, C=Cameroon" 
-
-# keytool \
-#   -genseckey \
-#   -keyalg HmacSHA512 \
-#   -keysize 512 \
-#   -keystore $KEYCLOAK_KEYSTORE_FILE \
-#   -storepass $KEYCLOAK_KEYSTORE_PASSWORD \
-#   -alias $KEYCLOAK_KEYSTORE_HMAC_SIG_KEY_ALIAS \
-#   -keypass $KEYCLOAK_KEYSTORE_PASSWORD \
-#   -storetype $KEYCLOAK_KEYSTORE_TYPE
-
-# keytool \
-#   -genseckey \
-#   -keyalg AES \
-#   -keysize 256 \
-#   -keystore $KEYCLOAK_KEYSTORE_FILE \
-#   -storepass $KEYCLOAK_KEYSTORE_PASSWORD \
-#   -alias $KEYCLOAK_KEYSTORE_AES_ENC_KEY_ALIAS \
-#   -keypass $KEYCLOAK_KEYSTORE_PASSWORD \
-#   -storetype $KEYCLOAK_KEYSTORE_TYPE 
 
 # Add concret info and passwords to key provider
 echo "Configuring ecdsa key provider..."
