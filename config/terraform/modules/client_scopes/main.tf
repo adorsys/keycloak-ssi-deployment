3b98b2f689f19a287566a9c3b712947914de289e
Awambeng Rodrick <awambengrodrick@gmail.com>
2025-09-10 15:07:11 +0100

    Fix clientscope Terraform module and clean up JSON configs

--- config/terraform/modules/client_scopes/main.tf
+++ config/terraform/modules/client_scopes/main.tf
@@ -6,26 +6,21 @@ terraform {
   }
 }
 
-
 locals {
-  identity_credential_json = templatefile("${path.root}/jsons/scopes/client-scope-identity_credential.json", {
-    realm_name = var.realm_name
-  })
-
-  steuerberater_credential_json = templatefile("${path.root}/jsons/scopes/client-scope-stbk_westfalen_lippe.json", {
-    realm_name = var.realm_name
-  })
-
-  kma_credential_json = templatefile("${path.root}/jsons/scopes/client-scope-kma_credential.json", {
-    realm_name = var.realm_name
-  })
+  identity_credential_json      = file("${path.root}/jsons/scopes/client-scope-identity_credential.json")
+  steuerberater_credential_json = file("${path.root}/jsons/scopes/client-scope-stbk_westfalen_lippe.json")
+  kma_credential_json           = file("${path.root}/jsons/scopes/client-scope-kma_credential.json")
 }
 
 resource "null_resource" "apply_custom_oid4vc_client_scopes" {
   depends_on = [var.realm_id]
 
   triggers = {
-    oid4vc_client_scopes_hash = join(",", [local.identity_credential_json, local.steuerberater_credential_json, local.kma_credential_json])
+    oid4vc_client_scopes_hash = join(",", [
+      sha256(local.identity_credential_json),
+      sha256(local.steuerberater_credential_json),
+      sha256(local.kma_credential_json)
+    ])
   }
 
   provisioner "local-exec" {
@@ -35,32 +30,40 @@ resource "null_resource" "apply_custom_oid4vc_client_scopes" {
       export KC_ADMIN_PASS="${var.admin_password}"
       export KC_URL="${var.keycloak_url}"
       export KC_REALM="master"
-      export TOKEN=$(curl -k -s -X POST "$KC_URL/realms/$KC_REALM/protocol/openid-connect/token" \
+
+      TOKEN=$(curl -k -s -X POST "$KC_URL/realms/$KC_REALM/protocol/openid-connect/token" \
         -H "Content-Type: application/x-www-form-urlencoded" \
         -d "client_id=admin-cli" \
         -d "username=$KC_ADMIN_USER" \
         -d "password=$KC_ADMIN_PASS" \
         -d "grant_type=password" | jq -r .access_token)
+
       echo "Importing OID4VC client scopes via curl..."
-      
+
       # Import IdentityCredential scope
-      echo '${local.identity_credential_json}' | curl -k -s -X POST "$KC_URL/admin/realms/${var.realm_name}/client-scopes" \
+      cat <<EOF | curl -k -s -X POST "$KC_URL/admin/realms/${var.realm_name}/client-scopes" \
         -H "Authorization: Bearer $TOKEN" \
         -H "Content-Type: application/json" \
         --data-binary @-
-      
+      ${local.identity_credential_json}
+      EOF
+
       # Import SteuerberaterCredential scope
-      echo '${local.steuerberater_credential_json}' | curl -k -s -X POST "$KC_URL/admin/realms/${var.realm_name}/client-scopes" \
+      cat <<EOF | curl -k -s -X POST "$KC_URL/admin/realms/${var.realm_name}/client-scopes" \
         -H "Authorization: Bearer $TOKEN" \
         -H "Content-Type: application/json" \
         --data-binary @-
+      ${local.steuerberater_credential_json}
+      EOF
 
       # Import KMACredential scope
-      echo '${local.kma_credential_json}' | curl -k -s -X POST "$KC_URL/admin/realms/${var.realm_name}/client-scopes" \
+      cat <<EOF | curl -k -s -X POST "$KC_URL/admin/realms/${var.realm_name}/client-scopes" \
         -H "Authorization: Bearer $TOKEN" \
         -H "Content-Type: application/json" \
         --data-binary @-
-      
+      ${local.kma_credential_json}
+      EOF
+
       echo "Custom OID4VC client scopes imported."
     EOT
     interpreter = ["bash", "-c"]
