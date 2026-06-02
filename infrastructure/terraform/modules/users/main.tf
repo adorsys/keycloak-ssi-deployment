addb09f54b0bfd81063e8c12db74e56e793853ec
Forkim Akwichek <forkimenjeckayang@gmail.com>
2026-06-02 11:41:38 +0000

    address review comments

--- infrastructure/terraform/modules/users/main.tf
+++ infrastructure/terraform/modules/users/main.tf
@@ -32,32 +32,3 @@ resource "keycloak_user" "max_mustermann" {
     temporary = false
   }
 }
-
-# Lookup the OpenID4VCI credential-offer-create realm role (required for credential offer creation)
-data "keycloak_role" "credential_offer_create" {
-  realm_id = var.realm_id
-  name     = "credential-offer-create"
-}
-
-# Assign the credential-offer-create realm role to the user
-resource "keycloak_user_roles" "francis_realm_roles" {
-  realm_id = var.realm_id
-  user_id  = keycloak_user.francis.id
-  # Keep default/composite realm roles intact; only ensure this role is present.
-  exhaustive = false
-
-  role_ids = [
-    data.keycloak_role.credential_offer_create.id,
-  ]
-}
-
-resource "keycloak_user_roles" "max_mustermann_realm_roles" {
-  realm_id = var.realm_id
-  user_id  = keycloak_user.max_mustermann.id
-  # Keep default/composite realm roles intact; only ensure this role is present.
-  exhaustive = false
-
-  role_ids = [
-    data.keycloak_role.credential_offer_create.id,
-  ]
-}
--- infrastructure/terraform/modules/users/variables.tf
+++ infrastructure/terraform/modules/users/variables.tf
@@ -33,31 +33,31 @@ variable "initial_password" {
 }
 
 variable "max_mustermann_username" {
-  description = "User name for the Max Mustermann payslip demo user"
+  description = "Username of the payslip demo user"
   type        = string
   default     = "max_mustermann"
 }
 
 variable "max_mustermann_first_name" {
-  description = "First name for the Max Mustermann payslip demo user"
+  description = "First name of the payslip demo user"
   type        = string
   default     = "Max"
 }
 
 variable "max_mustermann_last_name" {
-  description = "Last name for the Max Mustermann payslip demo user"
+  description = "Last name of the payslip demo user"
   type        = string
   default     = "Mustermann"
 }
 
 variable "max_mustermann_email" {
-  description = "Email for the Max Mustermann payslip demo user"
+  description = "Email of the payslip demo user"
   type        = string
   default     = "maxmustermann@mail.de"
 }
 
 variable "max_mustermann_initial_password" {
-  description = "Initial password for the Max Mustermann payslip demo user"
+  description = "Initial password of the payslip demo user"
   type        = string
   sensitive   = true
 }
